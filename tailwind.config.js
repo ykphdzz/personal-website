233747f615e87e6cd3f6afa5db905df1b9c0d76b
ykphdzz <67539986+ykphdzz@users.noreply.github.com>
2025-01-16 03:39:53 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg': '#f5f4f1',
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+      backgroundImage: {
+        'gradient-text': 'linear-gradient(to right, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
